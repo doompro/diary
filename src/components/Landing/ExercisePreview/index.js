09e173f20e9302239d4f8431bdc0c282e623fa1e
dsenigagliesi <d.senigagliesi@teamsystem.com>
2021-03-31 12:21:33 +0200

    fix: fix exercise title without sets

--- src/components/Landing/ExercisePreview/index.js
+++ src/components/Landing/ExercisePreview/index.js
@@ -1,16 +1,16 @@
-import React/*, { useState }*/ from "react";
+import React /*, { useState }*/ from "react";
 
 import { makeStyles } from "@material-ui/core/styles";
 
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
 
-import DeleteIcon from '@material-ui/icons/Delete';
-import CreateIcon from '@material-ui/icons/Create';
+import DeleteIcon from "@material-ui/icons/Delete";
+import CreateIcon from "@material-ui/icons/Create";
 
 /**
  * Exercise template:
- * 
+ *
  * db
  * -- userid
  * ------- date
@@ -21,8 +21,8 @@ import CreateIcon from '@material-ui/icons/Create';
  * ------- date
  * -------------- ex
  * -------------- ex
- * 
- * 
+ *
+ *
  * ex
  * ---- type: [ metcon, strength, mobility ]
  * ---- exercise: [ nome esercizio / descrizione metcon ]
@@ -31,50 +31,59 @@ import CreateIcon from '@material-ui/icons/Create';
  */
 
 const useStyles = makeStyles((theme) => ({
-    exerciseRoot: {
-        minHeight: 5,
-        minWidth: "80%",
-        padding: theme.spacing(2),
+  exerciseRoot: {
+    minHeight: 5,
+    minWidth: "80%",
+    padding: theme.spacing(2),
 
-        margin: "5px",
-    },
+    margin: "5px",
+  },
 
-    editIcon: {
-        marginRight: "10px",
-        cursor: 'pointer',
-    },
+  editIcon: {
+    marginRight: "10px",
+    cursor: "pointer",
+  },
 
-    deleteIcon: {
-        cursor: 'pointer',
-    }
+  deleteIcon: {
+    cursor: "pointer",
+  },
 }));
 
 const ExercisePreview = (props) => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    let exerciseRx = null;
-    if (props.exercise.rx) {
-        if (props.exercise.rx.sets && props.exercise.rx.reps) {
-            exerciseRx = " : " + props.exercise.rx.sets + " sets x " + props.exercise.rx.reps;
-        }
-        if (props.exercise.rx.load) {
-            exerciseRx = exerciseRx + " @ " + props.exercise.rx.load;
-        }
+  let exerciseRx = null;
+  if (props.exercise.rx) {
+    if (props.exercise.rx.sets && props.exercise.rx.reps) {
+      exerciseRx =
+        " : " +
+        (props.exercise.rx.sets || "") +
+        (props.exercise.rx.reps ? " sets x " + props.exercise.rx.reps : "");
     }
+    if (props.exercise.rx.load) {
+      exerciseRx = (exerciseRx || "") + " @ " + props.exercise.rx.load;
+    }
+  }
 
-    return (
-        <Paper className={classes.exerciseRoot}>
-            <Grid container direction="row" spacing={1} justify='space-between' >
-                <Grid item>
-                    {props.exercise.title + (exerciseRx ? exerciseRx : "")}
-                </Grid>
-                <Grid item>
-                    <CreateIcon onClick={() => props.onEdit(props.exercise)} className={classes.editIcon} />
-                    <DeleteIcon onClick={() => props.onRemove(props.exercise.id)} className={classes.deleteIcon} />
-                </Grid>
-            </Grid>
-        </Paper>
-    );
+  return (
+    <Paper className={classes.exerciseRoot}>
+      <Grid container direction="row" spacing={1} justify="space-between">
+        <Grid item>
+          {props.exercise.title + (exerciseRx ? exerciseRx : "")}
+        </Grid>
+        <Grid item>
+          <CreateIcon
+            onClick={() => props.onEdit(props.exercise)}
+            className={classes.editIcon}
+          />
+          <DeleteIcon
+            onClick={() => props.onRemove(props.exercise.id)}
+            className={classes.deleteIcon}
+          />
+        </Grid>
+      </Grid>
+    </Paper>
+  );
 };
 
 export default ExercisePreview;
