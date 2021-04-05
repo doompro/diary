3929a5269edf2b4afbce2fb0d2a71c6ed4811763
Daniele Senigagliesi <daniele.senigagliesi@gmail.com>
2021-04-05 21:13:29 +0200

    fix: fix open menu after login

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -10,15 +10,15 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
-//import MenuIcon from "@material-ui/icons/Menu";
 import CalendarToday from "@material-ui/icons/CalendarToday";
 import LockOpen from "@material-ui/icons/LockOpen";
 import ExitToApp from "@material-ui/icons/ExitToApp";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 import Build from "@material-ui/icons/Build";
-//import Switch from "@material-ui/core/Switch";
 import MenuItem from "@material-ui/core/MenuItem";
 import Menu from "@material-ui/core/Menu";
+
+import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -40,17 +40,7 @@ const useStyles = makeStyles((theme) => ({
 
 const Navigation = (firebase) => {
   const classes = useStyles();
-  const [anchorEl, setAnchorEl] = useState(null);
-  const open = Boolean(anchorEl);
   const history = useHistory();
-
-  const handleMenu = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
 
   const handleSignIn = () => {
     history.push(ROUTES.SIGN_IN);
@@ -81,55 +71,47 @@ const Navigation = (firebase) => {
                 </Typography>
 
                 {authUser ? (
-                  <>
-                    <IconButton
-                      aria-label="account of current user"
-                      aria-controls="menu-appbar"
-                      aria-haspopup="true"
-                      onClick={handleMenu}
-                      color="inherit"
-                    >
-                      <AccountCircle />
-                    </IconButton>
-                    <Menu
-                      id="menu-appbar"
-                      anchorEl={anchorEl}
-                      anchorOrigin={{
-                        vertical: "top",
-                        horizontal: "right",
-                      }}
-                      keepMounted
-                      transformOrigin={{
-                        vertical: "top",
-                        horizontal: "right",
-                      }}
-                      open={open}
-                      onClose={handleClose}
-                    >
-                      <MenuItem
-                        onClick={() => {
-                          history.push(ROUTES.ACCOUNT);
-                          handleClose();
-                        }}
-                      >
-                        <AccountCircle />
-                        <div className={classes.menuLabel}>Account</div>
-                      </MenuItem>
-                      {authUser.email === "daniele.senigagliesi@gmail.com" && (
-                        <MenuItem onClick={() => history.push(ROUTES.ADMIN)}>
-                          <Build />
-                          <div className={classes.menuLabel}>Admin</div>
-                        </MenuItem>
-                      )}
-                      <MenuItem
-                        className={classes.menuSeparator}
-                        onClick={() => { firebase.firebase.doSignOut().then(() => history.push(ROUTES.HOME)) }}
-                      >
-                        <ExitToApp />
-                        <div className={classes.menuLabel}>Log out</div>
-                      </MenuItem>
-                    </Menu>
-                  </>
+                  <PopupState variant="popover" popupId="demo-popup-menu">
+                    {(popupState) => (
+                      <>
+                        <IconButton
+                          aria-label="account of current user"
+                          aria-controls="menu-appbar"
+                          aria-haspopup="true"
+                          {...bindTrigger(popupState)}
+                          color="inherit"
+                        >
+                          <AccountCircle />
+                        </IconButton>
+                        <Menu
+                          {...bindMenu(popupState)}
+                        >
+                          <MenuItem
+                            onClick={() => {
+                              history.push(ROUTES.ACCOUNT);
+                              popupState.close();
+                            }}
+                          >
+                            <AccountCircle />
+                            <div className={classes.menuLabel}>Account</div>
+                          </MenuItem>
+                          {authUser.email === "daniele.senigagliesi@gmail.com" && (
+                            <MenuItem onClick={() => { history.push(ROUTES.ADMIN); popupState.close(); }}>
+                              <Build />
+                              <div className={classes.menuLabel}>Admin</div>
+                            </MenuItem>
+                          )}
+                          <MenuItem
+                            className={classes.menuSeparator}
+                            onClick={() => { firebase.firebase.doSignOut().then(() => { history.push(ROUTES.HOME); popupState.close(); }) }}
+                          >
+                            <ExitToApp />
+                            <div className={classes.menuLabel}>Log out</div>
+                          </MenuItem>
+                        </Menu>
+                      </>
+                    )}
+                  </PopupState>
                 ) : (
                   <IconButton
                     aria-label="Log in"
